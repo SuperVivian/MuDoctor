e4b883258ff52f8493140b40919d00c78d52a7a0
SuperVivian <1113679022@qq.com>
2020-12-27 08:20:09 +0800

    轮播图特效2

--- js/carousel.js
+++ js/carousel.js
@@ -10,6 +10,7 @@
     var right_btn = document.getElementById("right_btn");
     var circle_ol = document.getElementById("circle_ol");
     var circle_list = circle_ol.getElementsByTagName("li");
+    var banner = document.getElementById("banner");
 
     //clone第1个li
     var clone_li = carousel_list.firstElementChild.cloneNode(true);
@@ -18,7 +19,13 @@
     //idx就是当前显示的图片的索引
     var idx = 0;
 
-    right_btn.onclick = function() {
+    var lock = true;
+
+    right_btn.onclick = right_btn_handler;
+
+    function right_btn_handler() {
+        if (!lock) return;
+        lock = false;
         carousel_list.style.transition = "transform .5s ease 0s";
         idx++;
         carousel_list.style.transform = "translateX(" + (-16.66) * idx + "%)";
@@ -30,21 +37,44 @@
             idx = 0;
         }
         setCircles();
+        setTimeout(() => {
+            lock = true;
+        }, 500);
     }
+
+    var timer = setInterval(right_btn_handler, 2000);
+
+    banner.onmouseover = function() {
+        clearInterval(timer);
+    }
+
+    banner.onmouseleave = function() {
+        // 设表先关。防止用户频繁进出。
+        clearInterval(timer);
+        timer = setInterval(right_btn_handler, 2000);
+    }
+
+    var lock2 = true;
+
     left_btn.onclick = function() {
+        if (!lock2) return;
+        lock2 = false;
         if (idx == 0) {
             carousel_list.style.transition = "none";
             idx = 4;
+            carousel_list.style.transform = "translateX(" + (-16.66) * 5 + "%)";
             //小技巧：如果瞬间加上，相当于没加没去
             setTimeout(() => {
-                carousel_list.style.transition = "transform .5s ease 0s";
                 carousel_list.style.transform = "translateX(" + (-16.66) * 4 + "%)";
-
+                carousel_list.style.transition = "transform .5s ease 0s";
             }, 0);
         } else {
             idx--;
             carousel_list.style.transform = "translateX(" + (-16.66) * idx + "%)";
         }
+        setTimeout(() => {
+            lock2 = true;
+        }, 500);
         setCircles();
     }
     circle_ol.onclick = function(e) {
